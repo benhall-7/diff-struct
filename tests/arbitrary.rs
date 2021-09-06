9fdb6b3283e15cba67c7c339e1925d38acbf09fc
Dan Aloni <dan@kernelim.com>
2021-09-06 11:04:59 +0300

    Add tests/arbitrary

---
+++ tests/arbitrary.rs
@@ -0,0 +1,22 @@
+use quickcheck::quickcheck;
+use quickcheck_derive::Arbitrary;
+use std::collections::HashMap;
+use diff::Diff;
+
+#[derive(Clone, Arbitrary, Diff, Eq, PartialEq, Debug)]
+pub struct Basic {
+    pub items: HashMap<u16, Vec<u8>>,
+}
+
+fn tester<T>(mut a: T, b: T) -> bool
+    where T: Diff + Eq
+{
+    let diff = a.diff(&b);
+    a.apply(&diff);
+    return a == b;
+}
+
+#[test]
+fn test() {
+    quickcheck(tester as fn(Basic, Basic) -> bool);
+}
