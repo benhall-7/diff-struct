05177c470efd31e86cbfcbf4efedfea83fb57d92
Dan Aloni <dan@kernelim.com>
2021-09-29 07:56:29 +0300

    Support Arc

--- src/impls.rs
+++ src/impls.rs
@@ -4,6 +4,8 @@ use serde::{Serialize, Deserialize};
 use std::collections::{HashMap, HashSet};
 use std::fmt::{Debug, Formatter, Result as FmtResult};
 use std::hash::Hash;
+use std::sync::Arc;
+use std::ops::Deref;
 
 impl Diff for bool {
     type Repr = Option<bool>;
@@ -24,6 +26,31 @@ impl Diff for bool {
 
     fn identity() -> Self {
         false
+    }
+}
+
+impl<T> Diff for Arc<T>
+    where T: Diff + Clone
+{
+    type Repr = T::Repr;
+
+    fn diff(&self, other: &Self) -> Self::Repr {
+        self.deref().diff(other.deref())
+    }
+
+    fn apply(&mut self, diff: &Self::Repr) {
+        match Arc::get_mut(self) {
+            Some(m) => m.apply(diff),
+            None => {
+                let mut x = (**self).clone();
+                x.apply(diff);
+                *self = Arc::new(x);
+            }
+        }
+    }
+
+    fn identity() -> Self {
+        Arc::new(T::identity())
     }
 }
 
