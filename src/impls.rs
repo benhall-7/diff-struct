822ee71b5b831c2ed1eafbf797bec814d26d16a4
Dan Aloni <dan@kernelim.com>
2021-09-29 07:54:30 +0300

    Support BTreeMap

--- src/impls.rs
+++ src/impls.rs
@@ -1,7 +1,7 @@
 use super::utils::find_match;
 use super::*;
 use serde::{Serialize, Deserialize};
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, BTreeMap, BTreeSet};
 use std::fmt::{Debug, Formatter, Result as FmtResult};
 use std::hash::Hash;
 use std::sync::Arc;
@@ -277,90 +277,108 @@ pub struct HashMapDiff<K: Hash + Eq, V: Diff> {
     pub removed: HashSet<K>,
 }
 
-impl<K: Hash + Eq, V: Diff> Diff for HashMap<K, V>
-where
-    K: Clone,
-    V: PartialEq,
-{
-    type Repr = HashMapDiff<K, V>;
+/// The diff struct used to compare two HashMap's
+#[derive(Serialize, Deserialize)]
+#[serde(bound(serialize = "V::Repr: Serialize, K: Serialize"))]
+#[serde(bound(deserialize = "V::Repr: Deserialize<'de>, K: Deserialize<'de>"))]
+pub struct BTreeMapDiff<K: Ord + Eq, V: Diff> {
+    /// Values that are changed or added
+    pub altered: BTreeMap<K, <V as Diff>::Repr>,
+    /// Values that are removed
+    pub removed: BTreeSet<K>,
+}
 
-    fn diff(&self, other: &Self) -> Self::Repr {
-        let mut diff = HashMapDiff {
-            altered: HashMap::new(),
-            removed: HashSet::new(),
-        };
-        // can we do better than this?
-        for (key, value) in self {
-            if let Some(other_value) = other.get(key) {
-                // don't store values that don't change
-                if value != other_value {
-                    diff.altered.insert(key.clone(), value.diff(other_value));
+macro_rules! diff_map {
+    ($ty: ident, $diffty: ident, $diffkey: ident, ($($constraints:tt)*)) => {
+        impl<K: $($constraints)*, V: Diff> Diff for $ty<K, V>
+        where
+            K: Clone,
+            V: PartialEq,
+        {
+            type Repr = $diffty<K, V>;
+
+            fn diff(&self, other: &Self) -> Self::Repr {
+                let mut diff = $diffty {
+                    altered: $ty::new(),
+                    removed: $diffkey::new(),
+                };
+                // can we do better than this?
+                for (key, value) in self {
+                    if let Some(other_value) = other.get(key) {
+                        // don't store values that don't change
+                        if value != other_value {
+                            diff.altered.insert(key.clone(), value.diff(other_value));
+                        }
+                    } else {
+                        diff.removed.insert(key.clone());
+                    }
                 }
-            } else {
-                diff.removed.insert(key.clone());
+                for (key, value) in other {
+                    if let None = self.get(key) {
+                        diff.altered.insert(key.clone(), V::identity().diff(value));
+                    }
+                }
+                diff
+            }
+
+            // basically inexpensive
+            fn apply(&mut self, diff: &Self::Repr) {
+                diff.removed.iter().for_each(|del| {
+                    self.remove(del);
+                });
+                for (key, change) in &diff.altered {
+                    if let Some(original) = self.get_mut(key) {
+                        original.apply(change);
+                    } else {
+                        self.insert(key.clone(), V::identity().apply_new(change));
+                    }
+                }
+            }
+
+            fn identity() -> Self {
+                $ty::new()
             }
         }
-        for (key, value) in other {
-            if let None = self.get(key) {
-                diff.altered.insert(key.clone(), V::identity().diff(value));
+
+        impl<K: $($constraints)*, V: Diff> Debug for $diffty<K, V>
+        where
+            K: Debug,
+            V::Repr: Debug,
+        {
+            fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+                f.debug_struct(stringify!($diffty))
+                    .field("altered", &self.altered)
+                    .field("removed", &self.removed)
+                    .finish()
             }
         }
-        diff
-    }
 
-    // basically inexpensive
-    fn apply(&mut self, diff: &Self::Repr) {
-        diff.removed.iter().for_each(|del| {
-            self.remove(del);
-        });
-        for (key, change) in &diff.altered {
-            if let Some(original) = self.get_mut(key) {
-                original.apply(change);
-            } else {
-                self.insert(key.clone(), V::identity().apply_new(change));
+        impl<K: $($constraints)*, V: Diff> PartialEq for $diffty<K, V>
+        where
+            V::Repr: PartialEq,
+        {
+            fn eq(&self, other: &Self) -> bool {
+                self.altered == other.altered && self.removed == other.removed
+            }
+        }
+
+        impl<K: $($constraints)*, V: Diff> Clone for $diffty<K, V>
+        where
+            K: Clone,
+            V::Repr: Clone,
+        {
+            fn clone(&self) -> Self {
+                $diffty {
+                    altered: self.altered.clone(),
+                    removed: self.removed.clone(),
+                }
             }
         }
     }
-
-    fn identity() -> Self {
-        HashMap::new()
-    }
 }
 
-impl<K: Eq + Hash, V: Diff> Debug for HashMapDiff<K, V>
-where
-    K: Debug,
-    V::Repr: Debug,
-{
-    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
-        f.debug_struct("HashMapDiff")
-            .field("altered", &self.altered)
-            .field("removed", &self.removed)
-            .finish()
-    }
-}
-
-impl<K: Eq + Hash, V: Diff> PartialEq for HashMapDiff<K, V>
-where
-    V::Repr: PartialEq,
-{
-    fn eq(&self, other: &Self) -> bool {
-        self.altered == other.altered && self.removed == other.removed
-    }
-}
-
-impl<K: Eq + Hash, V: Diff> Clone for HashMapDiff<K, V>
-where
-    K: Clone,
-    V::Repr: Clone,
-{
-    fn clone(&self) -> Self {
-        HashMapDiff {
-            altered: self.altered.clone(),
-            removed: self.removed.clone(),
-        }
-    }
-}
+diff_map!(HashMap, HashMapDiff, HashSet, (Hash + Eq));
+diff_map!(BTreeMap, BTreeMapDiff, BTreeSet, (Ord));
 
 /// The type of change to make to a vec
 #[derive(Serialize, Deserialize)]
